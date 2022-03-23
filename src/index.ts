45eaaa6b9328f17a525cf93d8e345cba78c3df13
sam bacha <sam@manifoldfinance.com>
2022-03-23 00:31:30 -0700

    refactor(repo): reorg

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,69 @@
-import { getAddress } from '@ethersproject/address';
-import invariant from 'tiny-invariant';
-import warning from 'tiny-warning';
-import { BlockTag, FeeData, Provider, TransactionRequest, TransactionResponse } from "@ethersproject/abstract-provider";
-
-import { Deferrable, defineReadOnly, resolveProperties, shallowCopy } from "@ethersproject/properties";
-import {
-    Signer, VoidSigner,
-    // Types
-    ExternallyOwnedAccount
-} from "@ethersproject/abstract-signer";
-
-getAddress(): Promise<string> {
-    return Promise.resolve(this.address);
+export interface IResultOk<T> {
+  res: T;
+  err?: undefined;
 }
 
-const tx = shallowCopy(transaction);
-
-if (tx.from == null) {
-    tx.from = this.getAddress();
-
-} else {
-    // Make sure any provided address matches this signer
-    tx.from = Promise.all([
-        Promise.resolve(tx.from),
-        this.getAddress()
-    ]).then((result) => {
-        if (result[0].toLowerCase() !== result[1].toLowerCase()) {
-            logger.throwArgumentError("from address mismatch", "transaction", transaction);
-        }
-        return result[0];
-    });
+export interface IResultErr {
+  res?: undefined;
+  err: string;
 }
 
-// warns if addresses are not checksummed
-export function validateAndParseAddress(address: string): string {
-  try {
-    const checksummedAddress = getAddress(address);
-    warning(address === checksummedAddress, `${address} is not checksummed.`);
-    return checksummedAddress;
-  } catch (error) {
-    invariant(false, `${address} is not a valid address.`);
+export type TResult<T> = IResultOk<T> | IResultErr;
+
+export class Result<T> {
+  public static from<T>(value: TResult<T>) {
+    return new Result(value);
   }
-}
 
+  private readonly val: TResult<T>;
 
-export function validateAndParseAddress(address: string): string {
-    try {
-      const checksummedAddress = getFromAddress(address);
-      warning(address === checksummedAddress, `${address} is not checksummed.`);
-      return checksummedAddress;
-    } catch (error) {
-      invariant(false, `${address} is not a valid address.`);
-    }
+  constructor(value: TResult<T>) {
+    this.val = value;
   }
-  
-  
 
-export const isSameAddress = (addressOne?: TAddress, addressTwo?: TAddress) =>
-  isSameString(addressOne, addressTwo);
-
-
-
-  export function isNotWeth9Address(address: string): string {
-    try {
-      const weth9Address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-      warning(address === weth9Address, `${address} is the weth9 contract address.`);
-      return weth9Address;
-    } catch (error) {
-      invariant(false, `${address} is not a valid address.`);
+  public and<U>(func: (arg: T) => Result<U>) {
+    if (this.isOk(this.val)) {
+      const nextAssertable = func(this.val.res);
+      const nextVal = nextAssertable.toVal();
+      if (nextAssertable.isOk(nextVal)) {
+        return Result.from({ res: nextVal.res });
+      } else {
+        return Result.from<U>({
+          err: nextVal.err,
+        });
+      }
+    } else {
+      return Result.from<U>({ err: this.val.err });
     }
   }
 
-
-  
-
-export function isNotNullAddress(address: string): string {
-    try {
-      const nullAddress = '0x0000000000000000000000000000000000000000'
-      warning(address === nullAddress, `${address} is the null address.`);
-      return nullAddress;
-    } catch (error) {
-      invariant(false, `${address} is not a valid address.`);
+  public unwrap() {
+    if (this.isOk(this.val)) {
+      return this.val.res;
     }
+    throw Error(this.val.err);
   }
-  
+
+  public expect(msg: string) {
+    if (this.isOk(this.val)) {
+      return this.val.res;
+    }
+    throw Error(msg);
+  }
+
+  public ok() {
+    return !this.val.err;
+  }
+
+  public err() {
+    return this.val.err;
+  }
+
+  public toVal() {
+    return this.val;
+  }
+
+  private isOk(val: TResult<T>): val is IResultOk<T> {
+    return !val.err;
+  }
+}
