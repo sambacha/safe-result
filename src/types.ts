052a6e352e88f3ed747fb1953fc025e8d949e95b
sam bacha <sam@manifoldfinance.com>
2022-05-13 18:18:59 -0700

    feat(Ethers): signature

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+/** 
+* @interface Signature
+* @export {r,s,v}
+*/
+export interface Signature {
+  r: string
+  s: string
+  v: number
+}
+export type Bytes32 = string
+export type Uint16 = number
+export type Uint8 = number
+export type Uint24 = number
+export type Address = string
