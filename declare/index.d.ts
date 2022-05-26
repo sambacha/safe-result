a86f3abf856cf420da3a4aef1d9df80014cc681a
sam bacha <sam@manifoldfinance.com>
2022-05-25 23:50:00 -0700

    declare:interface

---
+++ declare/index.d.ts
@@ -0,0 +1,27 @@
+/** @interface EthereumProvider */
+export interface EthereumProvider {
+  on?: (...args: any[]) => void
+  removeListener?: (...args: any[]) => void
+  autoRefreshOnNetworkChange?: boolean
+}
+
+// declare global {
+//  interface Window {
+//    ethereum?: EthereumProvider
+//  }
+//}
+
+declare global {
+  interface Window {
+    ENV: any;
+    ethereum?: {
+      send: unknown;
+      isMetaMask?: true;
+      enable: () => Promise<string[]>;
+      on?: (...args: any[]) => void;
+      removeListener?: (...args: any[]) => void;
+      autoRefreshOnNetworkChange?: boolean;
+    };
+    web3?: Record<string, unknown>;
+  }
+}
