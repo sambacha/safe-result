14c690ed657043afe1f7afc5ea66f990e1fc724e
sam bacha <sam@manifoldfinance.com>
2022-05-25 23:33:28 -0700

    test(chains): all supported

---
+++ src/values/chain.test.ts
@@ -0,0 +1,31 @@
+/**
+* @fileName chain.test.ts
+* @test ALL_SUPPORTED_CHAIN_IDS
+*/
+
+import { ALL_SUPPORTED_CHAIN_IDS, SupportedChainId } from './ChainId';
+
+describe('chains', () => {
+  describe('ALL_SUPPORTED_CHAIN_IDS', () => {
+    it('contains all the values in the SupportedChainId enum', () => {
+      Object.values(SupportedChainId).forEach((chainId) => {
+        if (typeof chainId === 'number') expect(ALL_SUPPORTED_CHAIN_IDS.includes(chainId as number)).toBeTruthy()
+      })
+    });
+
+    it('contains no duplicates', () => {
+      const set = new Set<number>()
+      ALL_SUPPORTED_CHAIN_IDS.forEach((chainId) => {
+        expect(set.has(chainId)).toEqual(false)
+        set.add(chainId)
+      })
+    });
+
+    it('all values are in the SupportedChainId mapping', () => {
+      ALL_SUPPORTED_CHAIN_IDS.forEach((chainId) => {
+        // takes advantage of the reverse mapping
+        expect(SupportedChainId[chainId]).toBeTruthy()
+      })
+    })
+  })
+});
