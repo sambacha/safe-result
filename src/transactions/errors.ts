c6aa2789afcc06d6443ae2a3ff8b202be4f46334
sam bacha <sam@manifoldfinance.com>
2022-06-03 22:16:49 -0700

    build(ts): esnext (#11)

--- src/transactions/errors.ts
+++ src/transactions/errors.ts
@@ -1,364 +1,102 @@
-/**
- * @module ErrorHandling
- * @summary EthersJs ErrorCodes + Revert Parsing
- * @see {@link TODO}
- */
-import { Provider, TransactionReceipt } from "@ethersproject/providers"
-import { TransactionResponse } from "@ethersproject/abstract-provider"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+//import { TransactionResponse } from "@ethersproject/abstract-provider"
 import { toUtf8String } from "@ethersproject/strings";
+import { Provider, TransactionRequest } from "@ethersproject/providers";
 
-export type ErrorSignature = {
-    r: string;
-    s: string;
-    yParity: 0 | 1;
-    networkV: bigint;
+
+export const extractHostname = (url: string): string => {
+  let hostname: string;
+
+  if (url.indexOf("//") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(":")[0];
+  //find & remove "?"
+  hostname = hostname.split("?")[0];
+
+  return hostname;
 };
 
-export type ErrorAccessList = Array<{ address: string, storageKeys: Array<string> }>;
+export const getRevertReasonFromError = (error: any): string => {
+  return (
+    error.message?.split('reason="')[1]?.split('", code')[0] ??
+    error.error?.error?.message ??
+    "no error message"
+  );
+};
 
-export interface ErrorTransaction {
-    type?: number;
-    to?: string;
-    from?: string;
-    nonce?: number;
-    gasLimit?: bigint;
-    gasPrice?: bigint;
-    maxPriorityFeePerGas?: bigint;
-    maxFeePerGas?: bigint;
-    data?: string;
-    value?: bigint;
-    chainId?: bigint;
-    hash?: string;
-    signature?: ErrorSignature;
-    accessList?: ErrorAccessList;
-}
+export const decodeRevertReason = (code: string): string => {
+  try {
+    let codeString = `0x${code.substr(138)}`.replace(/0+$/, "");
+    if (codeString.length % 2 === 1) {
+      codeString += "0";
+    }
+    return toUtf8String(codeString);
+  } catch (error) {
+    return "Could not decode revert reason";
+  }
+};
 
-export interface ErrorFetchRequestWithBody extends ErrorFetchRequest {
-    body: Readonly<Uint8Array>;
-}
-
-export interface ErrorFetchRequest {
-    url: string;
-    method: string;
-    headers: Readonly<Record<string, string>>;
-    getHeader(key: string): string;
-    body: null | Readonly<Uint8Array>;
-    hasBody(): this is ErrorFetchRequestWithBody;
-}
-
-
-export interface ErrorFetchResponseWithBody extends ErrorFetchResponse {
-    body: Readonly<Uint8Array>;
-}
-
-export interface ErrorFetchResponse {
-    statusCode: number;
-    statusMessage: string;
-    headers: Readonly<Record<string, string>>;
-    getHeader(key: string): string;
-    body: null | Readonly<Uint8Array>;
-    hasBody(): this is ErrorFetchResponseWithBody;
-}
-
-/**
- * @export ErrorCode
- * @summary EthersJs ErrorCode
- */
-export type ErrorCode =
-
-    // Generic Errors
-    "UNKNOWN_ERROR" | "NOT_IMPLEMENTED" | "UNSUPPORTED_OPERATION" |
-    "NETWORK_ERROR" | "SERVER_ERROR" | "TIMEOUT" | "BAD_DATA" |
-
-    // Operational Errors
-    "BUFFER_OVERRUN" |  "NUMERIC_FAULT" |
-
-    // Argument Errors
-    "INVALID_ARGUMENT" | "MISSING_ARGUMENT" | "UNEXPECTED_ARGUMENT" |
-    "VALUE_MISMATCH" |
-
-    // Blockchain Errors
-    "CALL_EXCEPTION" | "INSUFFICIENT_FUNDS" | "NONCE_EXPIRED" |
-    "REPLACEMENT_UNDERPRICED" | "TRANSACTION_REPLACED" |
-    "UNPREDICTABLE_GAS_LIMIT" |
-    "UNCONFIGURED_NAME" | "OFFCHAIN_FAULT"
-;
-
-export interface EthersError<T extends ErrorCode = ErrorCode> extends Error {
-    code: ErrorCode;
-//    recover?: (...args: Array<any>) => any;
-    info?: Record<string, any>;
-    error?: Error;
-}
-
-// @note Generic Errors
-
-export interface UnknownError extends EthersError<"UNKNOWN_ERROR"> {
-    [ key: string ]: any;
-}
-
-export interface NotImplementedError extends EthersError<"NOT_IMPLEMENTED"> {
-    operation: string;
-}
-
-export interface UnsupportedOperationError extends EthersError<"UNSUPPORTED_OPERATION"> {
-    operation: string;
-}
-
-export interface NetworkError extends EthersError<"NETWORK_ERROR"> {
-    event: string;
-}
-
-export interface ServerError extends EthersError<"SERVER_ERROR"> {
-    request: ErrorFetchRequest | string;
-    response?: ErrorFetchResponse;
-}
-
-export interface TimeoutError extends EthersError<"TIMEOUT"> {
-    operation: string;
-    reason: string;
-    request?: ErrorFetchRequest;
-}
-
-export interface BadDataError extends EthersError<"BAD_DATA"> {
-    value: any;
-}
-
-
-// @note Operational Errors
-
-export interface BufferOverrunError extends EthersError<"BUFFER_OVERRUN"> {
-    buffer: Uint8Array;
-    length: number;
-    offset: number;
-}
-
-export interface NumericFaultError extends EthersError<"NUMERIC_FAULT"> {
-    operation: string;
-    fault: string;
-    value: any;
-}
-
-
-// Argument Errors
-
-export interface InvalidArgumentError extends EthersError<"INVALID_ARGUMENT"> {
-    argument: string;
-    value: any;
-    info?: Record<string, any>
-}
-
-export interface MissingArgumentError extends EthersError<"MISSING_ARGUMENT"> {
-    count: number;
-    expectedCount: number;
-}
-
-export interface UnexpectedArgumentError extends EthersError<"UNEXPECTED_ARGUMENT"> {
-    count: number;
-    expectedCount: number;
-}
-
-//export interface ValueMismatchError extends EthersError<ErrorCode.UNEXPECTED_ARGUMENT> {
-//    count: number;
-//    expectedCount: number;
-//}
-
-
-// Blockchain Errors
-
-export interface CallExceptionError extends EthersError<"CALL_EXCEPTION"> {
-    // The revert data
-    data: string;
-
-    // The transaction that triggered the exception
-    transaction?: any;
-
-    // The Contract, method and args used during invocation
-    method?: string;
-    signature?: string;
-    args?: ReadonlyArray<any>;
-
-    // The Solidity custom revert error
-    errorSignature?: string;
-    errorName?: string;
-    errorArgs?: ReadonlyArray<any>;
-    reason?: string;
-}
-
-//export interface ContractCallExceptionError extends CallExceptionError {
-    // The transaction call
-//    transaction: any;//ErrorTransaction;
-//}
-
-export interface InsufficientFundsError extends EthersError<"INSUFFICIENT_FUNDS"> {
-    transaction: any;//ErrorTransaction;
-}
-
-export interface NonceExpiredError extends EthersError<"NONCE_EXPIRED"> {
-    transaction: any; //ErrorTransaction;
-}
-
-export interface OffchainFaultError extends EthersError<"OFFCHAIN_FAULT"> {
-    transaction?: any;
-    reason: string;
-}
-
-export interface ReplacementUnderpricedError extends EthersError<"REPLACEMENT_UNDERPRICED"> {
-    transaction: any; //ErrorTransaction;
-}
-
-export interface TransactionReplacedError extends EthersError<"TRANSACTION_REPLACED"> {
-    cancelled: boolean;
-    reason: "repriced" | "cancelled" | "replaced";
-    hash: string;
-    replacement: any; //TransactionResponse;
-    receipt: any; //TransactionReceipt;
-}
-
-export interface UnconfiguredNameError extends EthersError<"UNCONFIGURED_NAME"> {
-    value: string;
-}
-
-export interface UnpredictableGasLimitError extends EthersError<"UNPREDICTABLE_GAS_LIMIT"> {
-    transaction: any; //ErrorTransaction;
-}
-
-
-// Coding; converts an ErrorCode its Typed Error
-/**
- * @export CodedEthersError
- */
-export type CodedEthersError<T> =
-    T extends "UNKNOWN_ERROR" ? UnknownError:
-    T extends "NOT_IMPLEMENTED" ? NotImplementedError:
-    T extends "UNSUPPORTED_OPERATION" ? UnsupportedOperationError:
-    T extends "NETWORK_ERROR" ? NetworkError:
-    T extends "SERVER_ERROR" ? ServerError:
-    T extends "TIMEOUT" ? TimeoutError:
-    T extends "BAD_DATA" ? BadDataError:
-
-    T extends "BUFFER_OVERRUN" ? BufferOverrunError:
-    T extends "NUMERIC_FAULT" ? NumericFaultError:
-
-    T extends "INVALID_ARGUMENT" ? InvalidArgumentError:
-    T extends "MISSING_ARGUMENT" ? MissingArgumentError:
-    T extends "UNEXPECTED_ARGUMENT" ? UnexpectedArgumentError:
-
-    T extends "CALL_EXCEPTION" ? CallExceptionError:
-    T extends "INSUFFICIENT_FUNDS" ? InsufficientFundsError:
-    T extends "NONCE_EXPIRED" ? NonceExpiredError:
-    T extends "OFFCHAIN_FAULT" ? OffchainFaultError:
-    T extends "REPLACEMENT_UNDERPRICED" ? ReplacementUnderpricedError:
-    T extends "TRANSACTION_REPLACED" ? TransactionReplacedError:
-    T extends "UNCONFIGURED_NAME" ? UnconfiguredNameError:
-    T extends "UNPREDICTABLE_GAS_LIMIT" ? UnpredictableGasLimitError:
-
-    never;
-
-/**
- * #isError
- * @param error 
- * @param code 
- * @returns 
- */
-export function isError<K extends ErrorCode, T extends CodedEthersError<K>>(error: any, code: K): error is T {
-    return (error && (<EthersError>error).code === code);
-}
-
-/**
- * #isCallException
- * @param error 
- * @returns 
- */
-export function isCallException(error: any): error is CallExceptionError {
-    return isError(error, "CALL_EXCEPTION");
-}
-
-/**
- * #getTransactionError
- * @param tx 
- * @param receipt 
- * @param provider 
- * @returns 
- */
-export const getTransactionError = async (
-    tx: TransactionResponse,
-    receipt: TransactionReceipt,
-    provider: Provider
+export const getRevertReasonFromTransaction = async (
+  provider: Provider,
+  tx: TransactionRequest,
+  blockNumber: number
 ): Promise<string> => {
-    if (typeof tx !== "object") {
-        throw TypeError(`tx argument ${tx} must be a transaction object`)
-    }
-    if (typeof receipt !== "object") {
-        throw TypeError(
-            `receipt argument ${receipt} must be a transaction receipt object`
-        )
-    }
-    if (receipt.status) {
-        throw TypeError(
-            "Transaction did not fail. Can only read the revert reason from failed transactions"
-        )
-    }
-    if (!receipt.transactionHash) {
-        throw TypeError(`There is no transaction hash on the receipt object`)
-    }
-    if (receipt.gasUsed === tx.gasLimit) {
-        throw Error("Transaction failed as it ran out of gas.")
-    }
+  let revertReason;
 
-    let rawMessageData
-    try {
-        const result = await provider.call(
-            {
-                ...tx,
-            },
-            receipt.blockNumber
-        )
+  try {
+    const code = await provider.call(tx, blockNumber);
+    let codeString = `0x${code.substr(138)}`.replace(/0+$/, "");
+    if (codeString.length % 2 === 1) {
+      codeString += "0";
+    }
+    revertReason = toUtf8String(codeString);
+  } catch (error: any) {
+    // Locally it will throw
+    revertReason = error.error?.message.split(
+      "VM Exception while processing transaction: revert "
+    )[1];
+  }
 
-        // Trim the 0x prefix
-        rawMessageData = result.slice(2)
-    } catch (e) {
-        if (e.message.startsWith("Node error: ")) {
-            // Trim "Node error: "
-            const errorObjectStr = e.message.slice(12)
-            // Parse the error object
-            const errorObject = JSON.parse(errorObjectStr)
+  return revertReason;
+};
 
-            if (!errorObject.data) {
-                throw Error(
-                    "Failed to parse data field error object:" + errorObjectStr
-                )
-            }
-
-            if (errorObject.data.startsWith("Reverted 0x")) {
-                // Trim "Reverted 0x" from the data field
-                rawMessageData = errorObject.data.slice(11)
-            } else if (errorObject.data.startsWith("0x")) {
-                // Trim "0x" from the data field
-                rawMessageData = errorObject.data.slice(2)
-            } else {
-                throw Error(
-                    "Failed to parse data field of error object:" +
-                        errorObjectStr
-                )
-            }
-        } else {
-            throw Error(
-                "Failed to parse error message from Ethereum call: " + e.message
-            )
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getSimplifiedErrorMessageFromEthersError = (error: any) => {
+  try {
+    const jsonError = JSON.parse(JSON.stringify(error));
+    if (jsonError.reason && jsonError.code) {
+      if (jsonError.error && jsonError.error.reason && jsonError.error.code) {
+        if (jsonError.error.serverError) {
+          return `reason: ${jsonError.reason}, code: ${
+            jsonError.code
+          }, error: { reason: ${jsonError.error.reason}, code: ${
+            jsonError.error.code
+          }, serverErrorCode: ${jsonError.error.serverError.code}${
+            jsonError.error.url
+              ? `, url: ${extractHostname(jsonError.error.url)}`
+              : ""
+          }}`;
         }
-    }
-
-    return parseReasonCode(rawMessageData)
-}
-
-export const parseReasonCode = (messageData: string): string => {
-    // Get the length of the revert reason
-    const strLen = parseInt(messageData.slice(8 + 64, 8 + 128), 16)
-    // Using the length and known offset, extract and convert the revert reason
-    const reasonCodeHex = messageData.slice(8 + 128, 8 + 128 + strLen * 2)
-    // Convert reason from hex to string
-    const reason = toUtf8String("0x" + reasonCodeHex)
-
-    return reason
+        return `reason: ${jsonError.reason}, code: ${
+          jsonError.code
+        }, error: { reason: ${jsonError.error.reason}, code: ${
+          jsonError.error.code
+        }${
+          jsonError.error.url
+            ? `, url: ${extractHostname(jsonError.error.url)}`
+            : ""
+        } }`;
+      }
+      return `reason: ${jsonError.reason}, code: ${jsonError.code}`;
+    } else return error.message;
+  } catch (e) {
+    return error.message;
+  }
 };
