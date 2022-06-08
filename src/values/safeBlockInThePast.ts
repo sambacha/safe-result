445c79181011eee67cb9ec0ccdbcfd786da899d6
sam bacha <sam@manifoldfinance.com>
2022-06-08 06:07:19 -0700

    chore(dev): add developer note

--- src/values/safeBlockInThePast.ts
+++ src/values/safeBlockInThePast.ts
@@ -1,5 +1,8 @@
 /**
 * @export safeBlockInThePast
 * @summary this should resolve CI errors probably caused by using a block too far in the past
+* @note value taken from
+*   {@link github.com/NomicFoundation/hardhat/blob/master/packages/hardhat-core/test/internal/hardhat-network/provider/interval-mining-provider.ts#L15}
 */
+
 export const safeBlockInThePast = 11_200_000;
