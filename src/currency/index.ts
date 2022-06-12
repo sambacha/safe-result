3b78d2c7b60f89cd957a2fd6186f7948c9195d13
sam bacha <sam@manifoldfinance.com>
2022-06-12 12:57:43 -0700

    feat: numbro

---
+++ src/currency/index.ts
@@ -0,0 +1,35 @@
+// currency library
+import numbro from 'numbro'
+
+export const formatDollarAmount = (num: number | undefined, digits = 2, round = true) => {
+  if (num === 0) return '$0.00'
+  if (!num) return '-'
+  if (num < 0.001 && digits <= 3) {
+    return '<$0.001'
+  }
+
+  return numbro(num).formatCurrency({
+    average: round,
+    mantissa: num > 1000 ? 2 : digits,
+    abbreviations: {
+      million: 'M',
+      billion: 'B',
+    },
+  })
+}
+
+export const formatAmount = (num: number | undefined, digits = 2) => {
+  if (num === 0) return '0'
+  if (!num) return '-'
+  if (num < 0.001) {
+    return '<0.001'
+  }
+  return numbro(num).format({
+    average: true,
+    mantissa: num > 1000 ? 2 : digits,
+    abbreviations: {
+      million: 'M',
+      billion: 'B',
+    },
+  })
+}
