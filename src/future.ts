43f5bfa120fdd75add071ff942f782b58e0fed8a
sam bacha <sam@manifoldfinance.com>
2022-07-14 09:47:03 -0700

    Feat: Future/FutureBoolean

---
+++ src/future.ts
@@ -0,0 +1,91 @@
+/** 
+* @export
+* @class Future
+*
+*/
+
+export type MaybeFuture<T> = T | Future<T>
+type UnpackFuture<T> = T extends Future<infer U> ? U : T
+export class Future<T> {
+  constructor(public resolve: () => T) {}
+
+  static resolve<T>(value: MaybeFuture<T>) {
+    return value instanceof Future ? value.resolve() : value
+  }
+
+  static create<T>(message = 'Trying to get value from unresolved future.') {
+    let resolved = false
+    let value: T
+    const future = new Future<T>(() => {
+      if (!resolved) {
+        throw new Error(message)
+      }
+      return value
+    })
+    return [
+      future,
+      (result: T) => {
+        resolved = true
+        value = result
+      },
+    ] as const
+  }
+
+  map<U>(fn: (value: T) => MaybeFuture<U>): Future<U> {
+    return new Future(() => Future.resolve(fn(this.resolve())))
+  }
+
+  get<U extends keyof T>(key: U): Future<UnpackFuture<T[U]>> {
+    return this.map((value) => value[key]) as any
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  equals(other: MaybeFuture<unknown>): FutureBoolean {
+    throw new Error('This will get overridden.')
+  }
+}
+
+type FutureTuple<T> = T extends [infer V, ...infer Rest] ? [Future<V>, ...FutureTuple<Rest>] : []
+
+type FutureArray<T> = T extends Array<any> ? FutureTuple<T> : Future<T>[]
+type UnpackArray<T> = T extends FutureArray<infer U> ? U : never
+type ForceArray<T> = T extends Array<any> ? T : T[]
+
+export function reduce<T extends FutureArray<any>, R>(
+  futures: T,
+  fn: (...args: ForceArray<UnpackArray<T>>) => R
+): Future<R> {
+  return new Future(() =>
+    Future.resolve(fn(...((futures as any[]).map((f) => f.resolve()) as ForceArray<UnpackArray<T>>)))
+  )
+}
+
+export type BooleanLike = MaybeFuture<boolean>
+
+/** 
+* @export
+* @class FutureBoolean
+*
+*/
+
+export class FutureBoolean extends Future<boolean> {
+  not() {
+    return new FutureBoolean(() => !this.resolve())
+  }
+
+  and(other: BooleanLike) {
+    return new FutureBoolean(() => this.resolve() && Future.resolve(other))
+  }
+
+  or(other: BooleanLike) {
+    return new FutureBoolean(() => this.resolve() || Future.resolve(other))
+  }
+
+  thenElse<T, U>(thenValue: MaybeFuture<T>, elseValue: MaybeFuture<U>): Future<T | U> {
+    return this.map((x) => (x ? Future.resolve(thenValue) : Future.resolve(elseValue)))
+  }
+}
+
+Future.prototype.equals = function (other: MaybeFuture<unknown>): FutureBoolean {
+  return new FutureBoolean(() => this.resolve() === Future.resolve(other))
+}
