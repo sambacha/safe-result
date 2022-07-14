7ebd8755cf54c3ad2fc8a5a2384616da27790f82
sam bacha <sam@manifoldfinance.com>
2022-07-14 09:48:07 -0700

    feat(lib): 0.8.1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 /**
 * @package safe-result
+* @version 0.8.1
 */
+
 export * from './chainable';
-export * from './maybe';
 export * from './result';
+export * from './maybe';
+export * from './future';
 export * from './timeout';
 export * from './error-handler';
 export * from './transactions';
